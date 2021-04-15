6241635b4f730fd2422d26dfc16165517c5ebf74
Kyle ZENG <jkjh1jkjh1@gmail.com>
2021-04-14 20:00:09 -0700

    cleanup executor

--- PhpIL/executor.py
+++ PhpIL/executor.py
@@ -3,15 +3,12 @@ import sys
 import json
 import logging
 import tempfile
-try:
-    import pwn
-except ImportError:
-    import subprocess
+from collections import defaultdict
+
+import pwnlib
 
 logger = logging.getLogger('Executor')
 logging.getLogger('pwnlib.tubes.process.process').setLevel(logging.ERROR)
-
-
 
 class Executor:
     '''
@@ -32,34 +29,29 @@ class Executor:
         self._output_dir = output_dir
         self._extra_args = [] if extra_args is None else extra_args
         self._is_stdin = is_stdin
-        self._code = code
-        self._non_zero_exits = {}
-    
-    @property
-    def code(self):
-        return self._code
-    
-    @code.setter
-    def code(self, code_obj):
-        self._code = code_obj
+        self._non_zero_exits = defaultdict(list)
+        self.code = code
     
     def execute(self, collect_stderr=False, save_input=False):
         logger.info("Executing %s" % self._program_path)
         assert collect_stderr is False, "Not Implemented!"
-        assert self._code is not None, "There is no code to run!"
+        assert self.code is not None, "There is no code to run!"
         if save_input is True:
             with open(f'{self._output_dir}/saved_input.php', 'w') as f:
-                f.write(self._code)
+                f.write(self.code)
+
+        # handle stdin cases
         if self._is_stdin:
             logger.info("Running with stdin")
-            return self._run_with_input()
-        else:
-            with tempfile.NamedTemporaryFile(mode='w', suffix='.php') as fp:
-                fp.write(self._code)
-                fp.seek(0)
-                fp.flush()
-                logger.info("Running with payload in temporary file %s" % fp.name)
-                return self._run_with_temp_file(fp.name)
+            return self._execute()
+
+        # handle non stdin cases
+        with tempfile.NamedTemporaryFile(mode='w', suffix='.php') as fp:
+            fp.write(self.code)
+            fp.seek(0)
+            fp.flush()
+            logger.info("Running with payload in temporary file %s" % fp.name)
+            return self._execute(filename=fp.name)
     
     def dump_inputs(self, filename='fuzzer_inputs.json'):
         if len(self._non_zero_exits) == 0:
@@ -68,53 +60,30 @@ class Executor:
         
         with open(f'{self._output_dir}/{filename}', 'w') as f:
             json.dump(self._non_zero_exits, f, indent=2)
-    
-    def _run_with_input(self):
-        command = ['ASAN_OPTIONS="coverage=1:coverage_dir=/tmp/coverages/"'] + self._build_command(stdin=True)
-        output, err = '', ''
-        if 'pwn' in sys.modules:
-            process = pwn.process(' '.join(x for x in command), shell=True)
-            process.sendline(self.code)
-            output = process.recvall().strip().decode('utf-8')
-        else:
-            process = subprocess.Popen(command, stdout=subprocess.PIPE, stdin=subprocess.PIPE)
-            output, err = process.communicate(self.code)
-            output = output.strip().decode('utf-8')
-            err = err.strip().decode('utf-8')
-        
-        return output, err
-    
-    def _run_with_temp_file(self, filename):
-        command = ['ASAN_OPTIONS="coverage=1:coverage_dir=/tmp/coverages/"'] + self._build_command(stdin=False, file_name=filename)
-        output, err = '', ''
-        if 'pwn' in sys.modules:
-            logger.info("Using pwn module")
-            process = pwn.process(' '.join(x for x in command), shell=True, alarm=5)
-            output = process.recvall().strip().decode('utf-8')
-            exit_code = process.poll()
-            logger.info("Received %d bytes of output" % len(output))
-        else:
-            logger.info("Using subprocess module")
-            process = subprocess.Popen(command, stdout=subprocess.PIPE)
-            output, err = process.communicate()
-            output = output.strip().decode('utf-8')
-            exit_code = process.returncode
-            logger.info("Received %d bytes of output" % len(output))
-            err = err.strip().decode('utf-8')
+
+
+    def _execute(self, filename=None):
+        cmd = self._build_command(filename=filename)
+
+        command = ['ASAN_OPTIONS="coverage=1:coverage_dir=/tmp/coverages/"'] + cmd
+        output, err, exit_code = b'', b'', -1
+
+        r = pwnlib.tubes.process.process(' '.join(x for x in command), shell=True, alarm=5)
+        output = r.recvall().strip().decode('utf-8')
+        exit_code = r.poll()
+        logger.info("Received %d bytes of output" % len(output))
         
         logger.info("Process exited with exit code %d" % exit_code)
         if exit_code != 0:
-            logger.info("Logging code")
-            if exit_code not in self._non_zero_exits:
-                self._non_zero_exits[exit_code] = []
-            self._non_zero_exits[exit_code].append({'input': self._code, 'output': output})
+            logger.info("Logging code with exit_code: %d", exit_code)
+            self._non_zero_exits[exit_code].append({'input': self.code, 'output': output})
 
-        return output, err
+        return output, err, exit_code
 
-    def _build_command(self, stdin=True, file_name=None):
-        if stdin is False:
-            assert file_name is not None, "File name is required when input is not from stdin"
-            return [f'{self._program_path}'] + self._program_args + [f'{file_name}'] + self._extra_args
-        else:
+    def _build_command(self, filename=None):
+        if self._is_stdin:
             return [f'{self._program_path}'] + self._program_args + self._extra_args
+
+        assert filename is not None, "File name is required when input is not from stdin"
+        return [f'{self._program_path}'] + self._program_args + [f'{filename}'] + self._extra_args
         
